37c87f883e86a388d60659aa80571979513c9121
Sng Poh Liang <87518191+dreamzspl@users.noreply.github.com>
2022-01-29 23:10:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,102 @@
+# React CRUD with Axios + API
+
+## Create local JSON server
+- Create `data.json` and insert data, e.g. from http://mockaroo.com 
+- Don't forget to remove last comma (`,`) in the array!
+### `data.json` format
+```json
+{
+  "contacts":  // endpoint
+  [
+    {
+      "id": 1, 
+      "name": "Shayne Eliasson",
+      "email": "seliasson0@fda.gov"
+    },
+    {
+      ...
+      // last item has no comma!
+    }  
+  ]
+}
+```
+Start JSON server with NPX
+```
+npx json-server --port 3300 --watch data.json
+```
+- Check JSON server is running by going to http://localhost:3300/contacts
+- JSON server data can have multiple endpoints by adding extra keys (e.g. "contacts") in the array
+- For example, add a new "recipes" key in `data.json` and access it with http://localhost:3300/recipes 
+
+## Create api modules
+- Create ./api directory
+- Create API.js
+- Create axios instance in API.js
+- Add `import API from './api/API.js'` to App.js file
+- Test axios is able to connect to JSON server
+
+## Start React app
+- Add apiGetContacts to fetch data from the app and test it using `console.log()`
+- Move apiGetContacts into `App.js`
+- Add and useEffect and useState in `App.js` to download data upon App component mount
+- Check component inspector to see state variables
+
+## Create Components According to Functionality
+
+### View
+> `ViewContactList.js`
+- Check props are correctly passed into component
+- Create JSX to generate table with `.map` for each row item
+- You can now view contact list in a table
+
+### Add
+> `App.js`
+- Create hooks `[formItem, setFormItem] = useState`
+- Create `handleSubmit` function for adding new contacts
+- Create JSX to generate input form with button
+- Create `newItem` state variable
+- Add `onChange={handleInput}` for each input field
+- Add `onClick={handleSubmit}` for submit button
+
+### Delete
+> `App.js`
+- Create `deleteItem` function
+> `ViewContactList.js`
+- Create Delete button and `deleteItem` as `onClick` handler
+
+### Edit
+> `App.js`
+- Create hooks `[isEditMode, setIsEditMode] = useState`
+- Edit function has 2 parts
+  - Edit contact form (`editItem`)
+  - Update edited item in contactList (`updateItem`)
+- Use isEditMode to switch between Add and Edit modes
+- Modify `handleSubmit` function to add Edit mode functions
+- Create Cancel button to clear form 
+> `ViewContactList.js`
+- Create Edit button and `props.editItem` as `onClick` handler
+
+## Building React + Axios with API Key
+
+- Create a .env.local file with the content:
+ `REACT_APP_API_KEY=<your_own_api_key>`
+- Add `.env.local` to `.gitignore` to exclude the secret api key file when you commit to Github
+
+> WARNING: 
+  - NOT RECOMMENDED for use in production code
+  - API key will still be visible in compiled code
+  - DO NOT check the .env.local file into Github repository
+
+```js
+ export const API2 = axios.create({
+   baseURL:'https://mboum-finance.p.rapidapi.com',
+   headers: {
+     'x-rapidapi-host': 'mboum-finance.p.rapidapi.com',
+     'x-rapidapi-key': process.env.REACT_APP_API_KEY
+   }    
+ })
+```
+
+Run `npm start` after adding/changing API key
+
+
---
+++ src/App.js
@@ -0,0 +1,147 @@
+import './App.css';
+import { uniqueId } from 'lodash';
+import { useState, useEffect } from 'react'
+import { API } from './api/API.js'
+import ViewContactList from './components/ViewContactList.js'
+
+function App() {
+  const blankForm = {
+    id: null,
+    name: '',
+    email: ''
+  }
+  const [contactList, setContactList] = useState([]);   // View function
+  const [formItem, setFormItem] = useState(blankForm);  // Add + Edit function
+  const [isEditMode, setIsEditMode] = useState(false);  // Edit function
+
+  const apiGetContacts = async () => {
+
+    // const res = await API.get('contacts');
+    // console.log('apiGetContacts, response:', res);
+    const { status, data } = await API.get('/contacts');
+    // console.log('apiGetContacts, data:', data);
+    if (status === 200) {
+      setContactList(data);
+    }
+  }
+/*
+  const apiGetStockPrices = async () => {
+    // const { status, data } = await API.get('/qu/quote?symbol=MSFT')
+    const { status, data } = await API.get('/hi/history?symbol=MSFT&interval=1d');
+    if (status === 200) {
+      console.log(data);
+    }
+  }
+*/
+  // Delete contact (Add async/wait for API)
+  const deleteItem = async (id) => { 
+    const newList = contactList.filter( item => item.id !== id );
+    setContactList(newList);
+    setIsEditMode(false); // For Edit function
+    try {
+      const response = await API.delete(`/contacts/${id}`);
+      console.log('API.delete response:', response);
+    } catch (err) {
+      console.log('API.delete error:', err.message);
+    }
+  }
+  // Edit contact form setup (1)
+  const editItem = item => {
+    console.log('editItem:', item);
+    setFormItem({
+      id: item.id,
+      name: item.name,
+      email: item.email
+    })
+    setIsEditMode(true);
+  }
+  // Handler for submit button
+  const handleSubmit = async () => {
+    console.log('handlerSubmit:');
+    // Update edited item (PUT)
+    if (isEditMode) {
+      const newList = contactList.map( item => item.id === formItem.id ? formItem : item );
+      setContactList(newList);
+      setIsEditMode(false);
+      try {
+        const response = await API.put(`/contacts/${formItem.id}`, formItem);
+        console.log('API.put response:', response);
+      } catch (err) {
+        console.log('API.put error:', err.message);
+      }  
+    } else {
+    // Add new item (POST)
+      const newItem = {...formItem, id: uniqueId('id')};
+      const newList = [newItem, ...contactList];
+      setContactList(newList);
+      // console.log('addItem:', newItem, newList);
+      try {
+        const response = await API.post('/contacts', newItem)
+        console.log('API.post response:', response);
+      } catch (err) {
+        console.log('API.post error:', err.message);
+      }
+    }
+    setFormItem(blankForm);
+  }
+  // Handler for input field boxes
+  const handleInput = e => {
+    const { name, value } = e.target;
+    const newItem = {...formItem, [name]: value}
+    setFormItem(newItem)
+  }
+  // Handler for cancel button
+  const handleCancel = e => {
+    console.log('handleCancel');
+    setFormItem(blankForm);
+    setIsEditMode(false);  
+  }
+
+  // Load contact list when component is mounted
+  useEffect( () => {
+    apiGetContacts();
+    // apiGetStockPrices();
+  }, [])
+  
+  return (
+    <div className="App container">
+      <h1>React CRUD with Axios + API</h1>
+      { /* Add conditional rendering for Edit function */
+        isEditMode 
+        ? <h2>Edit Contact</h2>
+        : <h2>Add Contact</h2>
+      }
+      <form>
+        <label>Name</label>
+        <input type="text" 
+          name="name" 
+          value={formItem.name}
+          onChange={handleInput} />
+        <label>Email</label>
+        <input type="email" 
+          name="email" 
+          value={formItem.email}
+          onChange={handleInput} />
+      </form>
+      <br />
+      <button onClick={handleSubmit} className="button primary">Submit</button>
+      { /* Add conditional rendering for Edit function */
+        isEditMode 
+        ? <>
+          <button onClick={handleCancel} className="button">Cancel</button>
+          <ViewContactList 
+            contactList={contactList} />
+          </>
+        : <> 
+          <br />
+          <ViewContactList 
+          contactList={contactList} 
+          deleteItem={deleteItem} 
+          editItem={editItem} />
+         </>
+        }
+    </div>
+  );
+}
+
+export default App;
